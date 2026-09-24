cbdb451ac62300e34eb5e7bb17b19fcda8658262
moritzbrantner <31051084+moritzbrantner@users.noreply.github.com>
2026-09-24 15:31:31 +0200

    test: reproduce browser reference video read failure

---
+++ playwright.pages.config.ts
@@ -0,0 +1,16 @@
+import baseConfig from './playwright.config'
+
+export default {
+  ...baseConfig,
+  retries: 0,
+  use: {
+    ...baseConfig.use,
+    baseURL: 'http://127.0.0.1:5175',
+  },
+  webServer: {
+    command: 'bun run --cwd site dev -- --hostname 127.0.0.1 --port 5175',
+    reuseExistingServer: false,
+    timeout: 120_000,
+    url: 'http://127.0.0.1:5175',
+  },
+}
